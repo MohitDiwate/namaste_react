ecb642b1df923d0a2b3b34c2ed204ec38d1008d0
Mohit@17 <mohitdiwate17@gmail.com>
2025-02-12 13:11:52 +0530

    episode-3

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "This is the First React Project"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "This is the First React Project"
+// );
 
+// console.log(heading);
+
+// const jsxHeading = <h1 id="jsxheading">Hello World</h1>;
+// console.log(jsxHeading);
+
+// Create a functional Component
+
+const link = <a href="">Click me</a>;
+const Title = <h1>{link}: React by NamasteDev.com 💎</h1>;
+
+const number = 2344;
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <h2>{console.log("sdkfhshdfhsdf")}</h2>
+      {Title}
+      <h1>Namaste React 🚀</h1>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("container"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
